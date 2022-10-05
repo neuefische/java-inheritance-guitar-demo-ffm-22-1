92c10377ae8c68e86395929a68843ce551056ba4
Sergej Grilborzer <sergej@grilborzer.de>
2022-10-05 08:37:10 +0200

    feat: Create basic Guitar class

---
+++ src/main/java/de/neuefische/Guitar.java
@@ -0,0 +1,16 @@
+package de.neuefische;
+
+public class Guitar {
+
+    String type = "acoustic";
+
+    public Guitar() { }
+
+    public Guitar(String type) {
+        this.type = type;
+    }
+
+    public String play() {
+        return "Todayyyy is gonna be the dayy";
+    }
+}
---
+++ src/main/java/de/neuefische/Main.java
@@ -0,0 +1,9 @@
+package de.neuefische;
+
+public class Main {
+    public static void main(String[] args) {
+        Guitar ibanez = new Guitar();
+
+        System.out.println(ibanez.play());
+    }
+}
